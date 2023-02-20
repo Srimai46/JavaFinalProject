e4667270a942764df3c75bdc6f5f2e4961b6e0fd
Srimai46 <Sira4r@gmail.com>
2023-02-20 17:53:52 +0700

    @2

---
+++ FinalProject/src/Waffle5.java
@@ -0,0 +1,192 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JRadioButton;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+
+public class Waffle5 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Waffle5 frame = new Waffle5();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Waffle5() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\D4bby\\Downloads\\0d1e70fd160476a3f78496be79e0813e.png"));
+		setTitle("Waffle Nanoom");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 700);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(254, 251, 205));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLarge = new JLabel("Large");
+		lblLarge.setForeground(new Color(255, 132, 72));
+		lblLarge.setFont(new Font("Yu Mincho", Font.BOLD, 60));
+		lblLarge.setBackground(new Color(255, 132, 72));
+		lblLarge.setBounds(355, 24, 175, 117);
+		contentPane.add(lblLarge);
+		
+		JRadioButton large_Origin = new JRadioButton("Original");
+		large_Origin.setHorizontalAlignment(SwingConstants.CENTER);
+		large_Origin.setFont(new Font("Yu Mincho", Font.BOLD, 28));
+		large_Origin.setBackground(new Color(254, 251, 205));
+		large_Origin.setForeground(new Color(255, 132, 72));
+		large_Origin.setBounds(301, 141, 149, 40);
+		contentPane.add(large_Origin);
+		
+		JRadioButton large_Choco = new JRadioButton("Chocolate");
+		large_Choco.setHorizontalAlignment(SwingConstants.CENTER);
+		large_Choco.setFont(new Font("Yu Mincho", Font.BOLD, 28));
+		large_Choco.setBackground(new Color(254, 251, 205));
+		large_Choco.setForeground(new Color(255, 132, 72));
+		large_Choco.setBounds(548, 188, 210, 93);
+		contentPane.add(large_Choco);
+		
+		JRadioButton large_Strawberry = new JRadioButton("Strawberry");
+		large_Strawberry.setHorizontalAlignment(SwingConstants.CENTER);
+		large_Strawberry.setFont(new Font("Yu Mincho", Font.BOLD, 28));
+		large_Strawberry.setBackground(new Color(254, 251, 205));
+		large_Strawberry.setForeground(new Color(255, 132, 72));
+		large_Strawberry.setBounds(295, 202, 202, 65);
+		contentPane.add(large_Strawberry);
+		
+		JRadioButton large_Green = new JRadioButton("Green Tea");
+		large_Green.setFont(new Font("Yu Mincho", Font.BOLD, 28));
+		large_Green.setHorizontalAlignment(SwingConstants.CENTER);
+		large_Green.setBackground(new Color(254, 251, 205));
+		large_Green.setForeground(new Color(255, 132, 72));
+		large_Green.setBounds(551, 133, 202, 57);
+		contentPane.add(large_Green);
+		
+		JLabel lblFlour = new JLabel("Flour :");
+		lblFlour.setForeground(new Color(255, 132, 72));
+		lblFlour.setFont(new Font("Yu Mincho", Font.BOLD, 32));
+		lblFlour.setBackground(new Color(255, 132, 72));
+		lblFlour.setBounds(137, 128, 119, 65);
+		contentPane.add(lblFlour);
+			
+		JLabel lblTopping = new JLabel("Topping :");
+		lblTopping.setForeground(new Color(255, 132, 72));
+		lblTopping.setFont(new Font("Yu Mincho", Font.BOLD, 32));
+		lblTopping.setBackground(new Color(255, 132, 72));
+		lblTopping.setBounds(91, 280, 163, 54);
+		contentPane.add(lblTopping);
+		
+		JComboBox large_Topping1 = new JComboBox();
+		large_Topping1.setFont(new Font("Yu Mincho", Font.BOLD, 26));
+		large_Topping1.setModel(new DefaultComboBoxModel(new String[] {"-", "Corn", "Crab strick", "Currant", "Foytong", "Pumpkin", "Coconut", "Taro", "Purple Sweet Potato", "Banana", "Chocolate chip", "Ham", "Cheddar Cheese", "Strawberryy Yam"}));
+		large_Topping1.setToolTipText("");
+		large_Topping1.setBounds(264, 288, 330, 30);
+		contentPane.add(large_Topping1);
+		
+		JLabel lblTopping_2 = new JLabel("Topping :");
+		lblTopping_2.setForeground(new Color(255, 132, 72));
+		lblTopping_2.setFont(new Font("Yu Mincho", Font.BOLD, 32));
+		lblTopping_2.setBackground(new Color(255, 132, 72));
+		lblTopping_2.setBounds(91, 335, 163, 54);
+		contentPane.add(lblTopping_2);
+		
+		JComboBox large_Topping2 = new JComboBox();
+		large_Topping2.setModel(new DefaultComboBoxModel(new String[] {"-", "Corn", "Crab strick", "Currant", "Foytong", "Pumpkin", "Coconut", "Taro", "Purple Sweet Potato", "Banana", "Chocolate chip", "Ham", "Cheddar Cheese", "Strawberryy Yam"}));
+		large_Topping2.setToolTipText("");
+		large_Topping2.setFont(new Font("Yu Mincho", Font.BOLD, 26));
+		large_Topping2.setBounds(264, 351, 330, 30);
+		contentPane.add(large_Topping2);
+		
+		JLabel lblNewLabel = new JLabel("Waffle Nanoom");
+		lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel.setForeground(new Color(255, 132, 72));
+		lblNewLabel.setFont(new Font("Yu Mincho", Font.BOLD, 18));
+		lblNewLabel.setBounds(85, 586, 163, 40);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\D4bby\\OneDrive\\เดสก์ท็อป\\photo gui\\0d1e70fd160476a3f78496be79e0813e-65x75.png"));
+		lblNewLabel_1.setBounds(10, 551, 65, 75);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblTopping_1 = new JLabel("Topping :");
+		lblTopping_1.setForeground(new Color(255, 132, 72));
+		lblTopping_1.setFont(new Font("Yu Mincho", Font.BOLD, 32));
+		lblTopping_1.setBackground(new Color(255, 132, 72));
+		lblTopping_1.setBounds(91, 399, 163, 54);
+		contentPane.add(lblTopping_1);
+		
+		JComboBox large_Topping3 = new JComboBox();
+		large_Topping3.setModel(new DefaultComboBoxModel(new String[] {"-", "Corn", "Crab strick", "Currant", "Foytong", "Pumpkin", "Coconut", "Taro", "Purple Sweet Potato", "Banana", "Chocolate chip", "Ham", "Cheddar Cheese", "Strawberryy Yam"}));
+		large_Topping3.setToolTipText("");
+		large_Topping3.setFont(new Font("Yu Mincho", Font.BOLD, 26));
+		large_Topping3.setBounds(264, 412, 330, 30);
+		contentPane.add(large_Topping3);
+		
+		JLabel lblTopping_1_1 = new JLabel("Topping :");
+		lblTopping_1_1.setForeground(new Color(255, 132, 72));
+		lblTopping_1_1.setFont(new Font("Yu Mincho", Font.BOLD, 32));
+		lblTopping_1_1.setBackground(new Color(255, 132, 72));
+		lblTopping_1_1.setBounds(91, 463, 163, 54);
+		contentPane.add(lblTopping_1_1);
+		
+		JComboBox large_Topping4 = new JComboBox();
+		large_Topping4.setModel(new DefaultComboBoxModel(new String[] {"-", "Corn", "Crab strick", "Currant", "Foytong", "Pumpkin", "Coconut", "Taro", "Purple Sweet Potato", "Banana", "Chocolate chip", "Ham", "Cheddar Cheese", "Strawberryy Yam"}));
+		large_Topping4.setToolTipText("");
+		large_Topping4.setFont(new Font("Yu Mincho", Font.BOLD, 26));
+		large_Topping4.setBounds(264, 479, 330, 30);
+		contentPane.add(large_Topping4);
+		
+		JLabel lblPer = new JLabel("10 ฿ per topping");
+		lblPer.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPer.setForeground(new Color(255, 132, 72));
+		lblPer.setFont(new Font("Yu Mincho", Font.BOLD, 16));
+		lblPer.setBackground(new Color(255, 132, 72));
+		lblPer.setBounds(301, 519, 257, 54);
+		contentPane.add(lblPer);
+		
+		JButton large_Next = new JButton("Next");
+		large_Next.setVerticalAlignment(SwingConstants.BOTTOM);
+		large_Next.setForeground(new Color(255, 132, 72));
+		large_Next.setFont(new Font("Yu Mincho", Font.BOLD, 28));
+		large_Next.setBackground(Color.WHITE);
+		large_Next.setBounds(418, 574, 194, 52);
+		contentPane.add(large_Next);
+		
+		JButton large_Back = new JButton("Back");
+		large_Back.setVerticalAlignment(SwingConstants.BOTTOM);
+		large_Back.setForeground(new Color(255, 132, 72));
+		large_Back.setFont(new Font("Yu Mincho", Font.BOLD, 28));
+		large_Back.setBackground(Color.WHITE);
+		large_Back.setBounds(648, 574, 194, 52);
+		contentPane.add(large_Back);
+	}
+}
---
+++ FinalProject/src/Waffletotal.java
@@ -0,0 +1,125 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+
+public class Waffletotal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Waffletotal frame = new Waffletotal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Waffletotal() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\D4bby\\Downloads\\0d1e70fd160476a3f78496be79e0813e.png"));
+		setTitle("Waffle Nanoom");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 700);
+		contentPane = new JPanel();
+		contentPane.setForeground(new Color(0, 0, 0));
+		contentPane.setBackground(new Color(254, 251, 205));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblQueue = new JLabel("Queue : 1");
+		lblQueue.setForeground(new Color(255, 132, 72));
+		lblQueue.setFont(new Font("Yu Mincho", Font.BOLD, 24));
+		lblQueue.setBounds(746, 10, 130, 52);
+		contentPane.add(lblQueue);
+
+		JLabel lblNewLabel = new JLabel("Waffle Nanoom");
+		lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel.setForeground(new Color(255, 132, 72));
+		lblNewLabel.setFont(new Font("Yu Mincho", Font.BOLD, 18));
+		lblNewLabel.setBounds(85, 586, 163, 40);
+		contentPane.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\D4bby\\OneDrive\\เดสก์ท็อป\\photo gui\\0d1e70fd160476a3f78496be79e0813e-65x75.png"));
+		lblNewLabel_1.setBounds(10, 551, 65, 75);
+		contentPane.add(lblNewLabel_1);
+
+		JLabel lblOrder = new JLabel("Bill");
+		lblOrder.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblOrder.setForeground(new Color(255, 132, 72));
+		lblOrder.setFont(new Font("Yu Mincho", Font.BOLD, 60));
+		lblOrder.setBounds(373, 31, 194, 137);
+		contentPane.add(lblOrder);
+
+		JLabel lblSize = new JLabel("1. {size}\r\n--------------------------------------------Price.");
+		lblSize.setFont(new Font("Yu Mincho", Font.PLAIN, 22));
+		lblSize.setBounds(73, 151, 620, 102);
+		contentPane.add(lblSize);
+
+		JLabel Total_Price = new JLabel("Total Price : {total}");
+		Total_Price.setFont(new Font("Yu Mincho", Font.PLAIN, 22));
+		Total_Price.setBounds(85, 474, 393, 102);
+		contentPane.add(Total_Price);
+
+		JLabel flout = new JLabel("- {flour}\r\n");
+		flout.setFont(new Font("Yu Mincho", Font.PLAIN, 22));
+		flout.setBounds(152, 217, 620, 102);
+		contentPane.add(flout);
+
+		JLabel topping = new JLabel("- {topping}\r\n--------------------------------Price.");
+		topping.setFont(new Font("Yu Mincho", Font.PLAIN, 22));
+		topping.setBounds(152, 288, 620, 102);
+		contentPane.add(topping);
+
+		JLabel discount = new JLabel("Discount : {discount}");
+		discount.setFont(new Font("Yu Mincho", Font.PLAIN, 22));
+		discount.setBounds(85, 423, 393, 102);
+		contentPane.add(discount);
+
+		JLabel Total = new JLabel("Total : {total}");
+		Total.setFont(new Font("Yu Mincho", Font.PLAIN, 22));
+		Total.setBounds(84, 373, 393, 102);
+		contentPane.add(Total);
+		
+		JButton btnbill_Close = new JButton("Close");
+		btnbill_Close.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnbill_Close.setForeground(new Color(255, 132, 72));
+		btnbill_Close.setFont(new Font("Yu Mincho", Font.BOLD, 28));
+		btnbill_Close.setBackground(Color.WHITE);
+		btnbill_Close.setBounds(648, 574, 194, 52);
+		btnbill_Close.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		contentPane.add(btnbill_Close);
+
+	}
+	
+}
